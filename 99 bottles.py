7a14d0559330d289158ce88abcffb5d49db4149d
Airtes00 <52472916+Airtes00@users.noreply.github.com>
2019-10-12 19:08:30 -0700

    Completely overhauled how the program runs with things I have learned about such as recursion, dictionaries and functions.

--- 99 bottles.py
+++ 99 bottles.py
@@ -1,25 +1,34 @@
-#Defined variables
+import sys
 
-beer = 99
-verse1 = "bottles of beer on the wall\n\n"
-verse2 = verse1[0:15] + "\n\n" #Prints only bottles of beer from verse 1
-verse3 = "take one down, pass it around \n\n"
-verse4 = verse1.replace("s", " ")# removes "S" from "beers"
-verse5 = verse4[0:15] + "\n\n" #same as verse 2 but no "s"
-
-if beer is not 0:
-    while beer > 1:
-        print(str(beer), verse1)
-        print(str(beer), verse2)
-        print(verse3)
-        beer -= 1
-        if beer is 1:
-            print(str(beer), verse4)
-        else:
-            print(str(beer), verse1)
+def bottles_song(beer):
+    #Holding the lyrics in a dictionary to
+    #take advantage of key value pairs.
+    lyrics = {
+        'v1':"bottles of beer on the wall!\n", #verse 1
+        'v2':"bottles of beer!\n", #verse 2
+        'v3':"take one down, pass it around!\n", #verse 3 
+        'v1nos':"bottle of beer on the wall!\n",  #verse 1 without the s in bottles
+        'v2nos':"bottle of beer!\n" #verse 2 without the s in bottles
+        }
+    if beer >= 2: #Do this if the value of beer is bigger or equal to 2
+        print(str(beer),lyrics.get('v1'),str(beer),lyrics.get('v2'),lyrics.get('v3'))
+        beer-=1
+        if beer == 1: # checks to see if beer changed to 1 and uses verse 1 without the s
+            print(str(beer),lyrics.get('v1nos'))
+            bottles_song(beer) #calls itself to run one final time.  
+        else:    
+            print(str(beer),lyrics.get('v1'))# if it passes the check it prints as normal
+            bottles_song(beer) # same as the top. This is mostly me practicing recursion its probably not the best I know.
     else:
-        print(str(beer), verse4)
-        print(str(beer), verse5)
-        print(verse3)
-        beer -= 1
-        print(str(beer), verse1)
+        print(str(beer),lyrics.get('v1nos'),str(beer),lyrics.get('v2nos'),lyrics.get('v3')) # final line for the last bottle
+        beer-=1
+        print(str(beer),lyrics.get('v1'))
+        sys.exit #exits because I am sure I just created a monster with bottles_song calling itself.
+
+beer = int(input(' How many beers do you have? and I will sing for each one! \n'))
+if beer == type(int):
+    if beer < 0:
+        print(" you can't have negative beers!")
+        sys.exit
+else:
+    bottles_song(beer)
